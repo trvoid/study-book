e285d670cf99e53cde6b534e067f0999fbf89a6e
Wayne Jeong <wayne.jeong.wj@gmail.com>
2020-01-03 16:50:10 +0900

    Select file and display memo contents.

--- renderer.js
+++ renderer.js
@@ -2,18 +2,43 @@ var ById = function(id) {
     return document.getElementById(id);
 }
 
+const {remote} = require('electron');
+const dialog = remote.dialog;
+const mainWindow = remote.getCurrentWindow();
+
 var jsonfile = require('jsonfile');
 var path = require('path');
 var studyBook = path.join(__dirname, 'sample.sbook');
 
 var materialArea = ById('material-area'),
-    material = ById('material-1');
+    material = ById('material-1'),
+    fileOpenButton = ById('file-open'),
     view = ById('view'),
-    studyArea = ById('study-area');
-    memo = ById('memo');
+    studyArea = ById('study-area'),
+    studyContent = ById('study-content');
 
 function openStudyBook(event) {
-    jsonfile.readFile(studyBook, function(err, obj) {
+    let options = {
+        title : 'Open file',
+        defaultPath : '.',
+        buttonLabel : 'Open',
+        filters : [
+            {name: 'Study books', extensions: ['sbook']}
+        ],
+        properties: ['openFile']
+    };
+
+    var filePaths = dialog.showOpenDialogSync(mainWindow, options);
+
+    if (filePaths === undefined) {
+        return;
+    }
+
+    while (studyContent.lastChild) {
+        studyContent.removeChild(studyContent.lastChild);
+    }
+
+    jsonfile.readFile(filePaths[0], function(err, obj) {
         if (err) {
             console.error(err);
             return;
@@ -23,10 +48,14 @@ function openStudyBook(event) {
         let link = obj.material.link;
         let study = obj.study;
         view.loadURL(link);
-        if (study.length !== 0) {
-            memo.innerText = study[0].contents;
+        for (s of study) {
+            var node = document.createElement('p');
+            var text = document.createTextNode(s.content);
+            node.appendChild(text);
+            studyContent.appendChild(node);
         }
     });
 }
 
 material.addEventListener('click', openStudyBook);
+fileOpenButton.addEventListener('click', openStudyBook);
