b0f7babe2d72ccebe70c9a1883f1b25d5203fd31
trvoid <wayne.jeong.wj@gmail.com>
2020-01-14 00:05:03 +0900

    Draggable study-area.

--- renderer.js
+++ renderer.js
@@ -103,6 +103,44 @@ function saveStudyNote() {
     });
 }
 
+function dragElement(elem) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elem.id + 'header')) {
+        document.getElementById(elem.id + 'header').onmousedown = dragMouseDown;
+    } else {
+        elem.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        elem.style.top = (elem.offsetTop - pos2) + 'px';
+        elem.style.left = (elem.offsetLeft - pos1) + 'px';
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Main                                                                       //
 ////////////////////////////////////////////////////////////////////////////////
@@ -130,3 +168,5 @@ switchHighlight.addEventListener('click', function() {
 });
 
 studyArea.style.display = 'none';
+
+dragElement(document.getElementById('study-area'));
